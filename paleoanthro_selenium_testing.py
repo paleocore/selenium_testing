8a101575d2f792cad0676f5759d48f60fd4dad0a
andrew <acmagee@gmail.com>
2015-02-23 15:54:33 -0600

    Began actually building the paleoanthro tests; structure for more test definitions now in place.

--- paleoanthro_selenium_testing.py
+++ paleoanthro_selenium_testing.py
@@ -1,2 +1,55 @@
 #!/usr/bin/env python 
 
+from selenium import webdriver
+from selenium_testing_util import Tester
+
+####################
+# Global variables #
+####################
+
+tester = Tester()
+
+####################
+# Test definitions #
+####################
+
+#
+## Home page tests
+#
+
+# Arbitrarily defining the banner links tests with the home tests
+def banner_links_nav_check():
+	links = (
+				('//nav[@id="mainmenu"]//a[text()="home"]', \
+					'http://www.paleoanthro.org/home/'),
+				('//nav[@id="mainmenu"]//a[text()="meetings"]', \
+					'http://www.paleoanthro.org/meetings/'),
+				('//nav[@id="mainmenu"]//a[text()="journal"]', \
+					'http://www.paleoanthro.org/journal/'),
+				('//nav[@id="mainmenu"]//a[text()="members"]', \
+					'http://www.paleoanthro.org/members/'),
+				('//nav[@id="mainmenu"]//a[text()="dissertations"]', \
+					'http://www.paleoanthro.org/dissertations/'),
+				('//nav[@id="mainmenu"]//a[text()="students"]', \
+					'http://www.paleoanthro.org/students/')
+			)
+
+	print 'banner_links_nav_check: ' + tester.links_nav_check(links)
+
+#######################
+# Page test groupings #
+#######################
+
+home = (banner_links_nav_check,)
+
+######################
+# Run configurations #
+######################
+
+browsers = (webdriver.Chrome,)
+pages = (
+			(home, 'http://www.paleoanthro.org/home/'),
+		)
+
+if __name__ == '__main__':
+	tester.run_test_suite(browsers, pages)
